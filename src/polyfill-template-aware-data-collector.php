69e37bad24b3dbef0af42462f5c5d0fea3b3ab12
GitHub Action <action@github.com>
2026-07-23 12:46:26 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- src/polyfill-template-aware-data-collector.php
+++ src/polyfill-template-aware-data-collector.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 use Symfony\Bundle\FrameworkBundle\DataCollector\TemplateAwareDataCollectorInterface;
 
-/**
+/*
  * Symfony 7.2+: TemplateAwareDataCollectorInterface was moved from HttpKernel to FrameworkBundle.
  * {@see BreadcrumbDataCollector} still implements the legacy FQCN for Symfony 6.4 compatibility.
  */
